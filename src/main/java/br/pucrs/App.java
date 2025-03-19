670d354298b28670d109d6fba06d1d441b0b2f76
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2025-03-19 19:59:37 -0300

    MaxVal

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -4,20 +4,34 @@ public class App
 {
     public static void main( String[] args )
     {
-        int[] arr = buildRandArray(32);
+        // int[] arr = buildRandArray(32);
 
-        System.out.println("Before sorting:");
-        printArray(arr);
+        // System.out.println("Before sorting:");
+        // printArray(arr);
 
-        arr = MergeSort.mergeSort(arr);
+        // arr = MergeSort.mergeSort(arr);
 
-        System.out.println("\nAfter sorting:");
-        printArray(arr);
-        
+        // System.out.println("\nAfter sorting:");
+        // printArray(arr);
+
+        long[] arr = buildRandArray(32);
+        System.out.println("Array size: " + arr.length);
+        long max1 = MaxVal.maxVal1(arr, arr.length);
+        long max2 = MaxVal.maxVal2(arr, arr.length);
+
+        long[] arr2 = buildRandArray(2048);
+        System.out.println("Array size: " + arr2.length);
+        max1 = MaxVal.maxVal1(arr2, arr2.length);
+        max2 = MaxVal.maxVal2(arr2, arr2.length);
+
+        long[] arr3 = buildRandArray(1048576);
+        System.out.println("Array size: " + arr3.length);
+        max1 = MaxVal.maxVal1(arr3, arr3.length);
+        max2 = MaxVal.maxVal2(arr3, arr3.length);
     }
 
-    private static int[] buildRandArray(int size) {
-        int[] arr = new int[size];
+    private static long[] buildRandArray(int size) {
+        long[] arr = new long[size];
         for (int i = 0; i < size; i++) {
             arr[i] = (int) (Math.random() * 10000);
         }
@@ -25,7 +39,7 @@ public class App
         return arr;
     }
 
-    private static void printArray(int[] arr) {
+    private static void printArray(long[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
---
+++ src/main/java/br/pucrs/MaxVal.java
@@ -0,0 +1,52 @@
+package br.pucrs;
+
+public class MaxVal {
+    private static long recursiveIterations;
+    
+    public static long maxVal1(long A[], int n) {
+        double startTime = System.nanoTime();
+        long iterations = 0;
+
+        long max = A[0];
+        for (int i = 1; i < n; i++) {  
+            iterations++;
+            if( A[i] > max ) 
+               max = A[i];
+        }
+
+        double endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1000000;
+        System.out.println("maxVal1 duration: " + duration + "ms");
+        System.out.println("maxVal1 iterations: " + iterations);
+        return max;
+    }
+
+    public static long maxVal2(long A[], int n) {
+        double startTime = System.nanoTime();
+        recursiveIterations = 0;
+        long result = maxVal2Helper(A, 0, n-1);
+        double endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1000000;
+        
+        System.out.println("maxVal2 duration: " + duration + "ms");
+        System.out.println("maxVal2 iterations: " + recursiveIterations);
+        return result;
+    }
+    
+    private static long maxVal2Helper(long A[], int init, int end) {
+        recursiveIterations++;
+        
+        if (end - init <= 1) {
+            return max(A[init], A[end]);  
+        } else {
+            int m = (init + end)/2;
+            long v1 = maxVal2Helper(A, init, m);   
+            long v2 = maxVal2Helper(A, m+1, end);  
+            return max(v1, v2);
+        }
+    }
+
+    public static long max(long a, long b) {
+        return (a > b) ? a : b;
+    }
+}
